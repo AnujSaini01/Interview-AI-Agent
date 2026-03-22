58bf716f73e459cd7ab44b38b0cd816aef0084b2
Anuj Saini <anujsaini@example.com>
2026-03-22 22:59:36 +0530

    Hardcode secure and sameSite true for cookies

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,12 +12,11 @@ export const googleAuth = async (req, res) => {
       });
     }
 
-    const isProduction = process.env.NODE_ENV === "production";
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge:7 * 24 * 60 * 60 * 1000,
     });
 
@@ -29,11 +28,10 @@ export const googleAuth = async (req, res) => {
 
 export const logOut = async (req, res) => {
   try {
-    const isProduction = process.env.NODE_ENV === "production";
     res.clearCookie("token", {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
     });
     return res.status(200).json({ message: "LogOut Successfully" });
   } catch (error) {
